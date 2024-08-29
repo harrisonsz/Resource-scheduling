ef2d6ce70c1301ab16015d5856b65ca64f7df3d8
zisun <sz1321.ic.ac.uk>
2024-08-29 15:25:30 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,73 @@
 
 struct EAPRTResult {
     double lastCompletedTime;
-    Snapshot newSnapshot;
+    Snapshot* newSnapshot;
 };
+
+class graph {
+public:
+    vector<Node*> nodes;
+    vector<Node*> sortedNodes;
+
+    void sortByRank() {
+        Node* startNode;
+        // find the start node of the graph
+        for (Node* node : nodes) {
+            if (node->parents.size() == 0) {
+                startNode = node;
+                break;
+            }
+        }
+        sortNodes(startNode);
+
+        // sort the nodes by rank
+        quickSort(sortedNodes, 0, sortedNodes.size() - 1, [](Node* a, Node* b) {
+            return a->rank < b->rank;
+        });
+    }
+
+    double sortNodes(Node* node) {
+        // if the node is exit node, the rank is its weight
+        if (node->children.size() == 0) {
+            node->rank = node->weight;
+            return node->rank;
+        } else {
+            double maxRank = 0;
+            for (Node* child : node->children) {
+                maxRank = max(maxRank, node->communicationCost + sortNodes(child));
+            }
+            return maxRank;
+        }
+    }
+
+    Node* pop() {
+        if (sortedNodes.size() == 0) {
+            return nullptr;
+        } else {
+            Node* node = sortedNodes[0];
+            sortedNodes.erase(sortedNodes.begin());
+            return node;
+        }
+    }
+
+    Snapshot snapshotNow() {
+        Snapshot snapshot;
+        for (Node* node : nodes) {
+            snapshot[node] = node->CRT;
+        }
+        return snapshot;
+    }
+
+    void restoreSnapshot(Snapshot snapshot) {
+        for (Node* node : nodes) {
+            node->CRT = snapshot[node];
+        }
+    }
+};
+
+
+
+
 
 class Node {
 public:
@@ -12,14 +77,12 @@ public:
     Device* device;
     Graph* graph;
     vector<Node*> parents;
+    vector<Node*> children;
     Channels* channels;
+    int rank;
 
     void setCRT(double CRT) {
         this->CRT = CRT;
-    }
-
-    void setDevice(Device device) {
-        this->device = device;
     }
 
     double getCRT() {
@@ -32,7 +95,8 @@ public:
     }
 
     EAPRTResult getEAPRT(Device* device) {
-        Snapshot originalSnapshot = graph->snapshotNow();
+        // record a snapshot of the current assignment of nodes to devices
+        Snapshot* originalSnapshot = graph->snapshotNow();
         for (Node* parent : parents) {
             if (parent->device == device) {
                 continue;
@@ -45,16 +109,17 @@ public:
                 parent->adjustCRT(outfeedStartTime);
             }
         }
-        Snapshot newSnapshot = graph->snapshotNow();
+        // record a new snapshot of the assignment of nodes to devices
+        Snapshot* newSnapshot = graph->snapshotNow();
         double lastCompletedTime = 0;
         for (Channel* infeed : channels->getInfeedChannels()) {
             lastCompletedTime = max(lastCompletedTime, infeed->getLastCompletedTime(parents));
         }
+        // turn the graph back to the state before the EAPRT calculation
         graph->restoreSnapshot(originalSnapshot);
+        // return the last completed time and the new snapshot
         return EAPRTResult{lastCompletedTime, newSnapshot};
     }
-
-
 };
 
 
@@ -66,17 +131,26 @@ int main() {
     while (node != nullptr) {
         double largestERT = 0;
         Device* largestERTDevice = nullptr;
+        Snapshot snapShotOfLargeERTAssignment;
+
+        // iterate the ERT of every device, find the device with the largest ERT
         for (Device* device : allDevices) {
-            double EAPRT = node->getEAPRT(device).lastCompletedTime;
-            double EST = max(device->availableKernel, EAPRT);
+            EAPRTResult EAPRTResult = node->getEAPRT(device);
+            double EST = max(device->availableKernel, EAPRTResult.lastCompletedTime);
             double ERT = max(device->availableOutfeed, node->weight + EST);
             if (ERT > largestERT) {
                 largestERT = ERT;
                 largestERTDevice = device;
+                snapShotOfLargeERTAssignment = EAPRTResult.newSnapshot;
             }
         }
-        node->setCRT(largestERT);
-        node->setDevice(largestERTDevice);
+        // Assign the largest ERT to the node ot make it CRT
+        node->CRT = largestERT;
+        // Assign the node to the device with the largest ERT
+        node->device = largestERTDevice;
+        // restore the snapshot of the assignment of nodes to devices
+        node->graph->restoreSnapshot(snapShotOfLargeERTAssignment);
+        // get the next node
         node = graph.pop();
     }
 
